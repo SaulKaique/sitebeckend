e88bd7d5743afdcae5198cdc6b210ea069befd0f
Saul kaiue <e107ti@etec.sp.gov.br>
2023-05-15 22:21:27 -0300

    Eu peguei o que fiz na aula de oca mas ainda não consegui fazer quase nada

---
+++ Login.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tela de login</title>
+    <link rel="stylesheet" href="login.css">
+</head>
+<body>
+    
+ 
+
+
+    
+
+    <form   class="Login-fundo"  action="insertLogin.php" method="post">
+
+        <img class="Usuario" src="Sistema/imagem/usuario2-1.png" alt=" imagem do usuario">
+
+        <label class="Longin" for="Longin">Longin</label>  
+        <input class="cxLongin" type="text" name="Longuin" id="Longin">
+        
+
+        <label class="Senha" for="Senha">Senha</label>
+        <input class="cxSenha" type="text" name="Senha" id="Senha">
+
+        <input class="Acessar" type="submit" value="Acessar">
+        <input  href="sistema/Cadastro-cliente"  class="Cadastro" type="submit" value="Cadastro">
+
+    
+        <img  class="marcapessoa" src="Sistema/imagem/coringapesso-1.png"   alt="minha marca pessoal a imagem do coringa">
+
+
+
+</form>
+
+
+
+
+</body>
+</html>
---
+++ Sistema/excadastro.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro</title>
+    <link rel="stylesheet" href="">
+</head>
+<body>
+
+   
+
+            <form class="fundo" action="insert.php">
+                
+
+               
+
+
+                <label class="Nome" for="Nome">Nome</label>  
+                <input class="cxNome" type="text" name="Nome" id="Nome">
+                
+        
+                <label class="E-mail" for="Senha">E-mail</label>
+                <input class="cxE-mail" type="text" name="E-mail" id="E-mail">
+        
+                <label class="Senha" for="Senha">Senha</label>
+               <input  class="Cxsenha"  type="password" name="Cxsenha" id="">
+
+                
+                <input class="Cadastro" type="submit" value="Cadastro">
+
+
+               
+
+
+            </form>
+
+            <img class="Usuario" src="imagem/usuario2-1.png" alt="Imagem de Usuario">
+
+            <img class="Marcapessoal" src="imagem/coringapesso-1.png" alt="logopessoal">
+
+
+</body>
+</html>
